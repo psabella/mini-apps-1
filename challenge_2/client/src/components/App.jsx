1089d236ec36f85e0c6bfbbcefab81b6d9c4cfa3
Pamela Sabella <psabella@Pamelas-Mac.local>
2020-01-14 13:17:04 -0800

    render text area

--- challenge_2/client/src/components/App.jsx
+++ challenge_2/client/src/components/App.jsx
@@ -7,11 +7,14 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>HELLO WORLD</div>
-
+      <div>
+        <div>HELLO WORLD, brought to you by my server and React, bitches.</div>
+        <textarea id="" name=""
+            rows="10" cols="75" placeholder="Enter your shit here.">
+        </textarea>
+      </div>
     )
   }
-
 }
 
 export default App;
